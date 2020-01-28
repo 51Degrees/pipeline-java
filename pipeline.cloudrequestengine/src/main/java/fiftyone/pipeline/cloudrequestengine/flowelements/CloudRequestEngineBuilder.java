16a63c64fbfb761b34965362b6fc4f5bd4b3fdaa
Ben Shillito <ben@51degrees.com>
2020-01-28 09:58:13 +0000

    BUG/TEST: Updated cloud request engine tests to add all required endpoints.

--- pipeline.cloudrequestengine/src/main/java/fiftyone/pipeline/cloudrequestengine/flowelements/CloudRequestEngineBuilder.java
+++ pipeline.cloudrequestengine/src/main/java/fiftyone/pipeline/cloudrequestengine/flowelements/CloudRequestEngineBuilder.java
@@ -3,7 +3,7 @@
  * Copyright 2019 51 Degrees Mobile Experts Limited, 5 Charlotte Close,
  * Caversham, Reading, Berkshire, United Kingdom RG4 7BY.
  *
- * This Original Work is licensed under the European Union Public Licence (EUPL) 
+ * This Original Work is licensed under the European Union Public Licence (EUPL)
  * v.1.2 and is subject to its terms as set out below.
  *
  * If a copy of the EUPL was not distributed with this file, You can obtain
@@ -13,10 +13,10 @@
  * amended by the European Commission) shall be deemed incompatible for
  * the purposes of the Work and the provisions of the compatibility
  * clause in Article 5 of the EUPL shall not apply.
- * 
- * If using the Work as, or as part of, a network application, by 
+ *
+ * If using the Work as, or as part of, a network application, by
  * including the attribution notice(s) required under Article 5 of the EUPL
- * in the end user terms of the application under an appropriate heading, 
+ * in the end user terms of the application under an appropriate heading,
  * such notice(s) shall fulfill the requirements of that article.
  * ********************************************************************* */
 
@@ -42,9 +42,10 @@ public class CloudRequestEngineBuilder extends
 
     private HttpClient httpClient;
 
-    private String dataEndpoint = "https://cloud.51degrees.com/api/v4/json";
-    private String propertiesEndpoint = "https://cloud.51degrees.com/api/v4/accessibleproperties";
-    private String evidenceKeysEndpoint = "https://cloud.51degrees.com/api/v4/evidencekeys";
+    private String endPoint = "https://cloud.51degrees.com/api/v4";
+    private String dataEndpoint = null;
+    private String propertiesEndpoint = null;
+    private String evidenceKeysEndpoint = null;
     private String resourceKey = null;
     private String licenseKey = null;
     private int timeout = 100000;
@@ -89,8 +90,9 @@ public class CloudRequestEngineBuilder extends
         if (uri.endsWith("/") == false) {
             uri += '/';
         }
-        return setDataEndpoint(uri + (licenseKey != null ? licenseKey + "." : "") + "json")
-            .setPropertiesEndpoint(uri + "accessibleproperties")
+        endPoint = uri;
+        return setDataEndpoint(uri + (resourceKey != null ? resourceKey + "." : "") + "json")
+            .setPropertiesEndpoint(uri + "accessibleproperties" + (resourceKey != null ? "?Resource=" +resourceKey : ""))
             .setEvidenceKeysEndpoint(uri + "evidencekeys");
 
     }
@@ -112,7 +114,7 @@ public class CloudRequestEngineBuilder extends
 
     public CloudRequestEngineBuilder setResourceKey(String resourceKey) {
         this.resourceKey = resourceKey;
-        return this;
+        return this.setEndpoint(endPoint);
     }
 
     public CloudRequestEngineBuilder setLicenseKey(String licenseKey) {
--- pipeline.cloudrequestengine/src/test/java/fiftyone/pipeline/cloudrequestengine/CloudRequestEngineTests.java
+++ pipeline.cloudrequestengine/src/test/java/fiftyone/pipeline/cloudrequestengine/CloudRequestEngineTests.java
@@ -3,7 +3,7 @@
  * Copyright 2019 51 Degrees Mobile Experts Limited, 5 Charlotte Close,
  * Caversham, Reading, Berkshire, United Kingdom RG4 7BY.
  *
- * This Original Work is licensed under the European Union Public Licence (EUPL) 
+ * This Original Work is licensed under the European Union Public Licence (EUPL)
  * v.1.2 and is subject to its terms as set out below.
  *
  * If a copy of the EUPL was not distributed with this file, You can obtain
@@ -13,10 +13,10 @@
  * amended by the European Commission) shall be deemed incompatible for
  * the purposes of the Work and the provisions of the compatibility
  * clause in Article 5 of the EUPL shall not apply.
- * 
- * If using the Work as, or as part of, a network application, by 
+ *
+ * If using the Work as, or as part of, a network application, by
  * including the attribution notice(s) required under Article 5 of the EUPL
- * in the end user terms of the application under an appropriate heading, 
+ * in the end user terms of the application under an appropriate heading,
  * such notice(s) shall fulfill the requirements of that article.
  * ********************************************************************* */
 
@@ -51,7 +51,7 @@ public class CloudRequestEngineTests {
     HttpClient httpClient;
     private TestLoggerFactory loggerFactory;
 
-    private URL expectedUrl = new URL("https://cloud.51degrees.com/api/v4/json");
+    private URL expectedUrl = new URL("https://cloud.51degrees.com/api/v4/resource_key.json");
 
     public CloudRequestEngineTests() throws MalformedURLException {
         ILoggerFactory internalLogger = mock(ILoggerFactory.class);
@@ -90,10 +90,10 @@ public class CloudRequestEngineTests {
             @Override
             public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
                 URL url = ((HttpURLConnection)invocationOnMock.getArgument(0)).getURL();
-                if (url.toString().endsWith("properties")) {
+                if (url.getPath().endsWith("properties")) {
                     return "{'Products': {}}";
                 }
-                else if (url.toString().endsWith("evidencekeys")) {
+                else if (url.getPath().endsWith("evidencekeys")) {
                     return "[]";
                 }
                 else {
