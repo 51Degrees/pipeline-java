03ffa73363483e59cb5c0081502c2e8a892b1426
Jo Rabin <jo@51degrees.com>
2023-03-03 11:57:21 +0000

    BUG: #5350 ShareUsageOverhead uses JUnit5 and tests need to be @Disabled not @Ignored

--- pipeline.engines.fiftyone/src/test/java/fiftyone/pipeline/engines/fiftyone/performance/ShareUsageOverheadTests.java
+++ pipeline.engines.fiftyone/src/test/java/fiftyone/pipeline/engines/fiftyone/performance/ShareUsageOverheadTests.java
@@ -32,7 +32,7 @@ import fiftyone.pipeline.engines.fiftyone.flowelements.ShareUsageElement;
 import fiftyone.pipeline.engines.flowelements.AspectEngine;
 import fiftyone.pipeline.engines.services.HttpClient;
 import fiftyone.pipeline.engines.testhelpers.flowelements.EmptyEngineBuilder;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.ILoggerFactory;
@@ -118,7 +118,7 @@ public class ShareUsageOverheadTests {
         return ((double)end - (double)start) / (double)iterations;
     }
 
-    @Test @Ignore
+    @Test @Disabled
     public void ShareUsageOverhead_SingleEvidence() {
         int iterations = 10000;
         List<FlowData> data = new ArrayList<>();
@@ -151,7 +151,7 @@ public class ShareUsageOverheadTests {
                     msOverheadPerCall < maxOverheadPerCall);
     }
 
-    @Test @Ignore
+    @Test @Disabled
     public void ShareUsageOverhead_ThousandEvidence() {
         int iterations = 1000;
         int evidenceCount = 1000;
