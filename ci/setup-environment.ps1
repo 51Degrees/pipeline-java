7f685b1c28ea8bd5eb308e359fac9f50a9ef079a
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-11 16:50:27 +0100

    Update setup-environment.ps1

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -11,11 +11,8 @@ Write-Host "Setting up $JavaSDKEnvVar"
 
 # Add the Java binary directory to the system PATH
 $env:Path = "$env:JAVA_HOME/bin;$env:Path"
-Write-Host $RUNNER_OS
-if($RUNNER_OS -eq Linux){
 
-    sudo ln -sf $env:JAVA_HOME/bin/java /usr/bin/java
-    }
+Write-Host $RUNNER_OS
 
 # Verify that the correct version of Java is being used
 java -version
