65f3e852be72417fc7ad955ec0e14509bf88acf2
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-11 15:21:27 +0100

    Update setup-environment.ps1

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -6,22 +6,7 @@ param(
 
 Write-Host "Setting up $JavaSDKEnvVar"
 
-# Get the current PATH and remove the old JAVA_HOME/bin directory
-$currentPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
-$oldJavaBinDir = Join-Path ([Environment]::GetEnvironmentVariable("JAVA_HOME")) "bin"
-$currentPath = $currentPath -replace "$oldJavaBinDir;", ""
-
-Write-Host $currentPath
-
-# Set the JAVA_HOME environment variable
-[Environment]::SetEnvironmentVariable('JAVA_HOME', [Environment]::GetEnvironmentVariable($JavaSDKEnvVar))
-
-# Add the Java binary directory to the system PATH
-$newJavaBinDir = Join-Path ([Environment]::GetEnvironmentVariable("JAVA_HOME")) "bin"
-$currentPath = $newJavaBinDir + ";" + $currentPath
-[Environment]::SetEnvironmentVariable('PATH', $currentPath, "Machine")
-
-Write-Host $currentPath
+Write-Host $env:GITHUB_PATH
 
 # Verify that the correct version of Java is being used
 java -version
