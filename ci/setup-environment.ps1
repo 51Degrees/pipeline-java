b2886b9ed7657487c02f637262bc402e7ff0ed41
Artur <artur@51degrees.com>
2023-04-11 15:10:51 +0100

    FEAT: Remove old path to java sdk from env variable

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -1,16 +1,27 @@
+
 param(
     [Parameter(Mandatory=$true)]
     [string]$JavaSDKEnvVar
 )
+
 Write-Host "Setting up $JavaSDKEnvVar"
+
+# Get the current PATH and remove the old JAVA_HOME/bin directory
+$currentPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+$oldJavaBinDir = Join-Path ([Environment]::GetEnvironmentVariable("JAVA_HOME")) "bin"
+$currentPath = $currentPath -replace "$oldJavaBinDir;", ""
+
+Write-Host $currentPath
+
 # Set the JAVA_HOME environment variable
-
-[Environment]::SetEnvironmentVariable('JAVA_HOME', [Environment]::GetEnvironmentVariable($JavaSDKEnvVar), 'Machine')
-
-Write-Host $env:JAVA_HOME
+[Environment]::SetEnvironmentVariable('JAVA_HOME', [Environment]::GetEnvironmentVariable($JavaSDKEnvVar))
 
 # Add the Java binary directory to the system PATH
-$env:Path += ";" + "$env:JAVA_HOME\bin"
+$newJavaBinDir = Join-Path ([Environment]::GetEnvironmentVariable("JAVA_HOME")) "bin"
+$currentPath = $newJavaBinDir + ";" + $currentPath
+[Environment]::SetEnvironmentVariable('PATH', $currentPath, "Machine")
+
+Write-Host $currentPath
 
 # Verify that the correct version of Java is being used
 java -version
