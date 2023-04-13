c1117fb805f08852ac3b360ed8b354805de23c80
Artur <artur@51degrees.com>
2023-04-13 11:18:00 +0100

    WIP

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -1,7 +1,9 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$Version
+    [hashtable]$Options
 )
+
+$Version = $Options.Version
 
 ./java/build-package-maven.ps1 -RepoName "pipeline-java-test" -Version $Version
 
