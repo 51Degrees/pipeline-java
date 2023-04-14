ce584a9fb3a799fa74218210430a89c43443cd99
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-14 15:04:39 +0100

    Update run-unit-tests.ps1

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -2,6 +2,6 @@ param(
     [string]$ProjectDir = "."
 )
 
-./java/run-unit-tests.ps1 -RepoName "pipeline-java" -ProjectDir $ProjectDir -Name ""
+./java/run-unit-tests.ps1 -RepoName "pipeline-java-test" -ProjectDir $ProjectDir -Name ""
 
 exit $LASTEXITCODE
