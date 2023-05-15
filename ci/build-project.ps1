9dfb7f00351d086bec163b3bf237f7233a107f34
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 13:19:02 +0100

    Update build-project.ps1

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -1,10 +1,12 @@
 param(
     [string]$ProjectDir = ".",
-    [string]$Name
+    [string]$Name,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
 )
 
 Write-Output "JAVA_HOME :$ENV:JAVA_HOME"
 
-./java/build-project.ps1 -RepoName "pipeline-java" -ProjectDir $ProjectDir -Name $Name
+./java/build-project.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name
 
 exit $LASTEXITCODE
